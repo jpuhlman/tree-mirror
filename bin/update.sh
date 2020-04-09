1d1630aaafa980359cb4136e7318de21290f4a44
Jeremy A. Puhlman <jpuhlman@mvista.com>
2020-04-09 15:56:55 -0700

    fix thinko

--- bin/update.sh
+++ bin/update.sh
@@ -68,6 +68,7 @@ git fetch --all
 if [ -n "$MASTER_TO_NEXT" ] ; then
     if [ -z "$(git branch -a | grep remotes\/origin/$MASTER_TO_NEXT)" ] ; then
         push origin $TO_REMOTE master $MASTER_TO_NEXT
+    fi
 fi
 
 if [ "$AUTO_PUSH_BASE" != "0" ] ; then
